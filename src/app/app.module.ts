b04ec624f3fbda5c17c37d981d0ff22adddd91b3
Aline Nink <alinenink@gmail.com>
2024-12-26 10:09:45 -0300

    doc files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,15 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent,
     AboutComponent,
     SkillsComponent,
     ProjectsComponent,
-    ContactComponent,],
-  imports: [BrowserModule],
+    ContactComponent],
+  imports: [BrowserModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
